951167246e1439314fee598a9a05a1cbf8bea58a
Andrei Guzei <aaguzey@gmail.com>
2024-01-08 01:01:56 +1000

    Initial commit

---
+++ src/main/java/com/example/securityjwtlearning/SecurityJwtLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityjwtlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtLearningApplication.class, args);
+	}
+
+}
